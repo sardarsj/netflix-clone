c7e935ebf7b2632b2a11f058bea83bedf18c81b6
sardarsj <sardarsjsingh08@gmail.com>
2023-09-13 18:32:13 +0530

    first commit

---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,20 @@
+import { Add, PlayArrow, ThumbDownOutlined, ThumbUpAltOutlined } from "@material-ui/icons"
+import "./listItem.scss"
+
+const ListItem = () => {
+  return (
+    <div className="listItem">
+      <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" alt="" />
+      <div className="itemInfo">
+        <div className="icons">
+            <PlayArrow/>
+            <Add/>
+            <ThumbUpAltOutlined/>
+            <ThumbDownOutlined/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ListItem
